2b56e5a03c4d5fa5222484eabb40246c7f809090
rishowrav <rishowrav@gmail.com>
2024-08-02 22:02:02 +0600

    Featcing data from MongoDB

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,35 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Card from "./components/Card";
+import axios from "axios";
+import useAxiosPublic from "./hooks/useAxiosPublic";
 
 export default function App() {
   const [asc, setAsc] = useState(true);
+  const [product, setProduct] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const axiosPublic = useAxiosPublic();
+
+  useEffect(() => {
+    getProductData();
+  }, []);
+
+  const getProductData = async () => {
+    await axiosPublic
+      .get("/products/")
+      .then((res) => {
+        setLoading(true);
+        setProduct(res.data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setLoading(false);
+      });
+  };
 
   return (
     <div className="space-y-10 mt-10">
+      {/* search and button */}
       <div className="flex justify-center gap-2">
         <div className="join">
           <input
@@ -23,22 +47,16 @@ export default function App() {
         </button>
       </div>
 
-      <div className="grid grid-cols-5 gap-2">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-      </div>
+      {/*  all card */}
+      {loading ? (
+        <h1 className="text-center text-2xl">Loading...</h1>
+      ) : (
+        <div className="grid grid-cols-5 gap-2">
+          {product.map((data) => (
+            <Card key={data?.product_id} data={data} />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,17 +1,16 @@
 import React from "react";
 
-const Card = () => {
+const Card = ({ data }) => {
   return (
     <div className="flex flex-col p-4 space-y-1 rounded-md dark:bg-gray-50 border">
       <div className="flex items-center justify-center flex-shrink-0 w-12 h-12 text-xl font-bold rounded-full dark:bg-indigo-600 dark:text-gray-50">
-        1
+        {data?.product_id}
       </div>
-      <h5 className="text-lg font-semibold ">Nostrum, corrupti blanditiis.</h5>
-      <p className="text-sm">
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex provident
-        numquam modi quos eaque voluptatibus praesentium.
+      <h5 className="text-lg font-semibold ">{data?.title}</h5>
+      <p className="text-sm">{data?.description}</p>
+      <p className="text-lg">
+        Price: <b> ${data?.price}</b>
       </p>
-      <p className="text-lg">Price: $45.00</p>
     </div>
   );
 };
