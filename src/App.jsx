6a752495e6bbdb22dca6b1b1b7a72e9bdd020ffd
rishowrav <rishowrav@gmail.com>
2024-08-02 15:15:39 +0600

    added: design part added

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,44 @@
+import { useState } from "react";
+import Card from "./components/Card";
+
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  const [asc, setAsc] = useState(true);
+
+  return (
+    <div className="space-y-10 mt-10">
+      <div className="flex justify-center gap-2">
+        <div className="join">
+          <input
+            className="input input-bordered join-item"
+            placeholder="Search"
+          />
+          <button className="btn btn-neutral join-item ">Search</button>
+        </div>
+
+        <button
+          onClick={() => setAsc(!asc)}
+          className="btn btn-active btn-accent"
+        >
+          {asc ? "Low to High" : "High to Low"}
+        </button>
+      </div>
+
+      <div className="grid grid-cols-5 gap-2">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
+    </div>
+  );
 }
